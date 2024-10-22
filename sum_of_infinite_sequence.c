64dca52635cc1ed1291ae9cfce8e29fd5a520043
d1gitale <nkalinin2020@gmail.com>
2024-10-22 16:39:56 +0300

    add the solution for the sum of infinite sequence

---
+++ sum_of_infinite_sequence.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+int GetInput (long double *p_input, char *prompt); 
+long double EvalSum (long double error);
+
+int main (void) {
+    long double error;
+    GetInput(&error, "Enter the error value: ");
+    printf("%.10Lf\n", EvalSum(error));
+    return 0;
+}
+
+int GetInput (long double *p_input, char *prompt) {
+    printf("%s", prompt);
+    while (scanf("%Lf", &*p_input) != 1) printf("%s", prompt);
+    return 0;
+}
+
+long double EvalSum (long double error) {
+    int i, factorial;
+    i = 1;
+    factorial = 1;
+    long double old_sum, cur_sum, unit;
+    old_sum = 0;
+    cur_sum = 0;
+
+    do {
+        old_sum = cur_sum;
+        factorial *= i;
+        unit = (long double) 1 / factorial;
+        cur_sum += unit;
+        i++;
+    } while (cur_sum - old_sum > error);
+    
+    return cur_sum;
+}
+
